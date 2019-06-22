b6979057c9488cd99f9489dc1c7d06bcff9f6e75
trulepa <trulepa@mail.ru>
2019-06-22 19:17:33 +0400

    Добавляет скрипт меню и стили шапке для разрешений 320, 768, 1200.

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,17 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+function init(event) {
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  var nav__list = document.querySelector('.site-nav__list');
+  var nav__toggle = document.querySelector('.site-nav__toggle');
+
+  nav__toggle.classList.remove('site-nav__toggle--nojs');
+  nav__list.classList.remove('site-nav__list--active');
+
+  nav__toggle.addEventListener('click', function () {
+    nav__list.classList.toggle('site-nav__list--active');
+  });
+
+}
+
+window.addEventListener("load", init);
