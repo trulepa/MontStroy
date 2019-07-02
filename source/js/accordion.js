290a1066b105638e2136a5ad0ad09f2b2645f3ea
trulepa <trulepa@mail.ru>
2019-07-02 19:02:43 +0400

    Правки по ревью

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,32 +1,34 @@
-function init(event) {
+'use strict';
+
+function init() {
 
   var $item = document.getElementsByClassName('services-list__item');
 
   for (var i = 0; i < $item.length; i++) {
-    $item[i].addEventListener("click", accordion);
+    $item[i].addEventListener('click', accordion);
   }
 }
 
 
 function accordion(event) {
 
-  if (event.target.classList.contains("services-list__title")) {
+  if (event.target.classList.contains('services-list__btn')) {
 
     var $item = document.getElementsByClassName('services-list__item');
     for (var i = 0; i < $item.length; i++) {
 
-      if ($item[i].classList.contains("services-list__item--show")) {
-        $item[i].classList.toggle("services-list__item--show");
+      if ($item[i].classList.contains('services-list__item--show')) {
+        $item[i].classList.toggle('services-list__item--show');
 
-        if ($item[i] != this) {
-          this.classList.toggle("services-list__item--show");
+        if ($item[i] !== this) {
+          this.classList.toggle('services-list__item--show');
         }
         return;
       }
     }
-    this.classList.toggle("services-list__item--show");
+    this.classList.toggle('services-list__item--show');
 
   }
 }
 
-window.addEventListener("load", init);
+window.addEventListener('load', init);
